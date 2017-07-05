1d52c9e41d871384b63a1ae3cb089e93f29da1f5
Koen <keetron@gmail.com>
2017-07-05 10:47:33 +0200

    Added karate tests

---
+++ src/test/java/karate/getHelloWorld.java
@@ -0,0 +1,11 @@
+package karate;
+
+import org.junit.runner.RunWith;
+import com.intuit.karate.junit4.Karate;
+import cucumber.api.CucumberOptions;
+
+@RunWith(Karate.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber"})
+public class getHelloWorld {
+
+}
